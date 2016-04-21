ddf73724f360430b3f4b9c75cd42001538d992de
chan <sittiporn.suks@bumail.net>
2016-04-21 11:57:42 +0700

    ฉันได้ทำอินเทรนไปยังหน้าเมนเรียบร้อย

--- app/src/main/java/mean/chan/mind/sendgps/FirstActivity.java
+++ app/src/main/java/mean/chan/mind/sendgps/FirstActivity.java
@@ -1,7 +1,9 @@
 package mean.chan.mind.sendgps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    } //main mathod
+
+    public void clickAddPlate(View view) {
+        startActivity(new Intent(FirstActivity.this,MainActivity.class));
     }
-}
+} //main class
